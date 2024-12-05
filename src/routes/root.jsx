8c78e3fc79a34ebacddb4b497fe903e5a2d7515b
N4h0 <89570972+N4h0@users.noreply.github.com>
2024-12-05 13:21:39 +0100

    Fiksa CSP, fjerna chatbot.
    Rar oppdatering I CSP-greier eller noko, måtte legge inn hashes for alle inline styles brukt.

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,20 +1,16 @@
 import React from 'react';
 import NavBar from '../components/NavBar'; // Adjust path as necessary
 import Footer from '../components/Footer'; // Adjust path as necessary
-import Chatbot from '../components/Chatbot'; // Adjust path as necessary
 import { Outlet } from "react-router-dom";
 import './root.css';
-
-
 
 const Layout = ({ children }) => {
     return (
         <>
-            <NavBar />
+            <NavBar /> 
             <div className = "App" id="detail">
                 <Outlet />
             </div>
-            <Chatbot />
             <Footer />
         </>
     );
